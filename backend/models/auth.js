df911f1b1b85e9e678bcf046f806bc1b9067d842
IK <khushijukanti33@gmail.com>
2024-08-15 15:56:07 +0530

    updated file name

---
+++ backend/models/auth.js
@@ -0,0 +1,31 @@
+// models/user.js
+const bcrypt = require('bcryptjs');
+const dynamoDb = require('../config/db');
+
+const TABLE_NAME = 'Users';
+
+const User = {
+  async createUser(username, email, password) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const params = {
+      TableName: TABLE_NAME,
+      Item: {
+        email,
+        username,
+        password: hashedPassword,
+      },
+    };
+    await dynamoDb.put(params).promise();
+  },
+
+  async findUserByEmail(email) {
+    const params = {
+      TableName: TABLE_NAME,
+      Key: { email },
+    };
+    const result = await dynamoDb.get(params).promise();
+    return result.Item;
+  },
+};
+
+module.exports = User;
