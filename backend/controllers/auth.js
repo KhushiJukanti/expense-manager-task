0992125f7736e64a4819be3ebcc2c9f68d3685c4
IK <khushijukanti33@gmail.com>
2024-08-15 15:54:12 +0530

    update require path

---
+++ backend/controllers/auth.js
@@ -0,0 +1,32 @@
+// controllers/authController.js
+const jwt = require('jsonwebtoken');
+const User = require('../models/auth');
+const bcrypt = require('bcryptjs');
+
+exports.register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+    await User.createUser(username, email, password);
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Error registering user', error });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findUserByEmail(email);
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, {
+      expiresIn: '1h',
+    });
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Error logging in', error });
+  }
+};
