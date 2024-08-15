4f88a1d5a830cc1b3f377e0e09daa4d03d903002
IK <khushijukanti33@gmail.com>
2024-08-15 16:57:43 +0530

    update routes

--- backend/routes/expense.js
+++ backend/routes/expense.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middlewares/auth');
 
 const router = express.Router();
 
-router.post('/expenses', authMiddleware, addExpense);
+router.post('/expenses',authMiddleware, addExpense);
 router.get('/expenses', authMiddleware, getExpenses);
 router.put('/expenses/:id', authMiddleware, updateExpense);
 router.delete('/expenses/:id', authMiddleware, deleteExpense);
