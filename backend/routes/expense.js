6e3fcdfdf23d4cfd9827837425a9391abaf1770c
IK <khushijukanti33@gmail.com>
2024-08-28 18:08:46 +0530

    create edit expense page for update expenses from backend

--- backend/routes/expense.js
+++ backend/routes/expense.js
@@ -1,13 +1,13 @@
 // routes/expenseRoutes.js
 const express = require('express');
 const { addExpense, getExpenses, updateExpense, deleteExpense } = require('../controllers/expense');
-const authMiddleware = require('../middlewares/auth');
+// const authMiddleware = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/expenses', addExpense);
 router.get('/expenses', getExpenses);
-router.put('/expenses/:id', authMiddleware, updateExpense);
+router.put('/expenses/:id', updateExpense);
 router.delete('/expenses/:id', deleteExpense);
 
 module.exports = router;
